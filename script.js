8a2c6ba57e7865ca9c90335050893d436e65ca8d
patokinder3413 <109300294+patokinder3413@users.noreply.github.com>
2022-07-14 12:01:55 -0400

    initial comit

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿const accounts = [', constraseña: 123', 'Cuenta de Disney+: mail: ejemplo@gmail.com, constraseña: 123', 'Cuenta de Amazon Prime video: mail: ejemplo@gmail.com, constraseña: 123']
+
+function generateRandomAccount() {
+  const randomAccount = accounts[Math.floor(Math.random() * accounts.length)];
+  alert(randomAccount)
+}
